3bbae2c94570b72b7982974f9518fdb385d81051
Simon Erhardt <me@rootlogin.ch>
2017-01-28 12:32:42 +0100

    initial commit

---
+++ armhero_build_alpine_3_4.groovy
@@ -0,0 +1,52 @@
+#!groovy
+folder('armhero') {
+    displayName('ARMhero')
+    description('ARMhero Projects')
+}
+job('armhero/build.alpine-3.4') {
+  label('rpi3')
+  logRotator {
+    numToKeep(30)
+    artifactNumToKeep(1)
+    daysToKeep(-1)
+    artifactDaysToKeep(-1)
+  }
+  wrappers {
+    preBuildCleanup()
+    timestamps()
+    colorizeOutput()
+    credentialsBinding {
+      usernamePassword('DOCKER_USERNAME', 'DOCKER_PASSWORD', '1d448f61-46d6-4af8-a517-9a06866447bb')
+    }
+  }
+  scm {
+    git {
+      remote {
+        url('git@code.dini-mueter.net:armhero/alpine.git')
+        branches('*/master')
+        credentials('8ffaa0c1-6e5d-4884-b2ee-854685476789')
+      }
+    }
+  }
+  triggers {
+    scm('H/5 * * * *')
+    cron('H 4 * * 0')
+  }
+  steps {
+    shell('sudo ARCH=armhf ./build.sh -r v3.4')
+    shell('''
+    docker login -u \044{DOCKER_USERNAME} -p \044{DOCKER_PASSWORD}
+
+    docker tag armhero/alpine:latest armhero/alpine:3.4
+    docker push armhero/alpine:latest
+    docker rmi armhero/alpine:latest
+
+    docker push armhero/alpine:3.4
+    docker rmi armhero/alpine:3.4
+    ''')
+  }
+  publishers {
+    mailer('me@rootlogin.ch')
+    wsCleanup()
+  }
+}
---
+++ armhero_build_alpine_edge.groovy
@@ -0,0 +1,44 @@
+#!groovy
+job('armhero/build.alpine-edge') {
+  label('rpi3')
+  logRotator {
+    numToKeep(30)
+    artifactNumToKeep(1)
+    daysToKeep(-1)
+    artifactDaysToKeep(-1)
+  }
+  wrappers {
+    preBuildCleanup()
+    timestamps()
+    colorizeOutput()
+    credentialsBinding {
+      usernamePassword('DOCKER_USERNAME', 'DOCKER_PASSWORD', '1d448f61-46d6-4af8-a517-9a06866447bb')
+    }
+  }
+  scm {
+    git {
+      remote {
+        url('git@code.dini-mueter.net:armhero/alpine.git')
+        branches('*/master')
+        credentials('8ffaa0c1-6e5d-4884-b2ee-854685476789')
+      }
+    }
+  }
+  triggers {
+    scm('H/5 * * * *')
+    cron('H 4 * * *')
+  }
+  steps {
+    shell('sudo ARCH=armhf ./build.sh -r edge -t edge')
+    shell('''
+    docker login -u \044{DOCKER_USERNAME} -p \044{DOCKER_PASSWORD}
+
+    docker push armhero/alpine:edge
+    docker rmi armhero/alpine:edge
+    ''')
+  }
+  publishers {
+    mailer('me@rootlogin.ch')
+    wsCleanup()
+  }
+}
