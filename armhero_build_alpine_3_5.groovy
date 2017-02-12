591ec9c7a259991594d2161f10e0bf9b657ed5d4
Simon Erhardt <me@rootlogin.ch>
2017-02-12 15:03:35 +0100

    added job for alpine 3.5

---
+++ armhero_build_alpine_3_5.groovy
@@ -0,0 +1,47 @@
+#!groovy
+job('armhero/build.alpine-3.5') {
+  label('pr-armv7')
+  logRotator {
+    numToKeep(30)
+    artifactNumToKeep(1)
+    daysToKeep(-1)
+    artifactDaysToKeep(-1)
+  }
+  wrappers {
+    preBuildCleanup()
+    timestamps()
+    colorizeOutput()
+    credentialsBinding {
+      usernamePassword('DOCKER_USERNAME', 'DOCKER_PASSWORD', '1d448f61-46d6-4af8-a517-9a06866447bb')
+    }
+  }
+  scm {
+    git {
+      remote {
+        url('git@github.com:armhero/alpine.git')
+        branches('*/master')
+        credentials('8ffaa0c1-6e5d-4884-b2ee-854685476789')
+      }
+    }
+  }
+  triggers {
+    scm('H/5 * * * *')
+    cron('H 5 * * 0')
+  }
+  steps {
+    shell('sudo ARCH=armhf ./build.sh -r v3.5 -t 3.5')
+    shell('''
+    sudo docker login -u \044{DOCKER_USERNAME} -p \044{DOCKER_PASSWORD}
+
+    sudo docker push armhero/alpine:3.5
+    sudo docker rmi armhero/alpine:3.5
+
+    # Access Microbadger Github
+    curl -X POST https://hooks.microbadger.com/images/armhero/alpine/ufmYLRNfn7Uj_sXNIW2SGuEg6Qo=
+    ''')
+  }
+  publishers {
+    mailer('me@rootlogin.ch')
+    wsCleanup()
+  }
+}
